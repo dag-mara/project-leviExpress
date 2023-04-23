8584e07fa51a50ab82934e7fde4088f9bdbb184b
Dagmar Suchá <78727813+dag-mara@users.noreply.github.com>
2023-04-23 14:59:15 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,6 @@ import './style.css';
 
 export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
-//createRoot(document.querySelector('#app')).render(<App />, );
-
 const router = createBrowserRouter([
     {
       path: '/',
